7f0241d9f63d379804d65d57bf6290bed57b2dad
Putra Ramditiana <147238721+PutraRamdit@users.noreply.github.com>
2024-03-11 10:04:04 +0000

    Ganti quote

---
+++ main.py
@@ -0,0 +1,18 @@
+#membuat variabel
+nama = "putra"
+semester = 2
+kelas = "TI 23 B"
+jurusan = "Teknik Informatika"
+quote = "Belajar tidak ada batasan"
+menikah = False
+adalahPria = True
+hobi = ["futsal","main game"]
+
+print("Nama saya : ",nama)
+print("Semester : ",semester)
+print("Kelas : ",kelas)
+print("Jurusan : ",jurusan)
+print("Quote :",quote)
+print("Status pernikahan :",menikah)
+print("Jenis kelamin : pria",adalahPria)
+print("Hobi :",hobi)
